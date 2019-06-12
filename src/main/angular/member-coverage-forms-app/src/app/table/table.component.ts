8760a09ed4bab63f68a5cc7a62d1fed5dfd41a8a
Evan Trout <evan.trout@sunlife.com>
2019-06-12 15:08:08 -0500

    Add filter form fields and some styling

--- src/main/angular/member-coverage-forms-app/src/app/table/table.component.ts
+++ src/main/angular/member-coverage-forms-app/src/app/table/table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 import { Form } from '../form';
 import { MOCKFORMS } from "../../assets/mockForms";
 
@@ -10,6 +11,27 @@ import { MOCKFORMS } from "../../assets/mockForms";
 export class TableComponent implements OnInit {
 
   forms: Form[];
+
+  coverageTypesVar = ['STD', 'LTD', 'DENTAL'];
+
+  statesVar = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
+    'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI',
+    'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY',
+    'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT',
+    'WA', 'WI', 'WV', 'WY'];
+
+  sourceSystemsVar = ['SOLAR', 'QPS'];
+
+  formTypeVar = ['Claim', 'Continuance'];
+
+  tableFilters = new FormGroup({
+    formType: new FormControl(''),
+    coverageType: new FormControl(''),
+    states: new FormControl(''),
+    name: new FormControl(''),
+    description: new FormControl(''),
+    link: new FormControl('')
+  });
 
   constructor() { }
 
