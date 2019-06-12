7cbdc05a92239bb9d82462846a3fb8d971e7320e
Evan Trout <evan.trout@sunlife.com>
2019-06-12 10:34:41 -0500

    Add table component

--- src/main/angular/member-coverage-forms-app/src/app/app.module.ts
+++ src/main/angular/member-coverage-forms-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {RouterModule, Routes} from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SubmissionFormComponent } from './submission-form/submission-form.component';
@@ -12,8 +12,8 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 const appRoutes: Routes = [
   {path: 'submission-form', component: SubmissionFormComponent},
   {path: 'search-form', component: SearchComponent},
-  {path: '', redirectTo: '/submission-form', pathMatch: 'full'},
   {path: 'table', component: TableComponent},
+  {path: '', redirectTo: '/submission-form', pathMatch: 'full'},
 ];
 
 @NgModule({
