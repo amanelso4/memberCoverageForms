2c5647063ba8e94d2e6613903d09ed4a4eaddf4d
KT43 <amanda.x.nelson@sunlife.com>
2019-06-13 07:43:53 -0500

    Updated page navigation through buttons and minor bugs fixed

--- src/main/angular/member-coverage-forms-app/src/app/form.ts
+++ src/main/angular/member-coverage-forms-app/src/app/form.ts
@@ -9,4 +9,3 @@ export class Form {
     public description: string,
   ) {    }
 }
-
--- src/main/angular/member-coverage-forms-app/src/app/submission-form/forms.service.ts
+++ src/main/angular/member-coverage-forms-app/src/app/submission-form/forms.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Form } from './form';
+import { Form } from '../form';
 //**import { catchError } from 'rxjs/operators';
 //**import { HttpErrorHandler, HandleError } from '../http-error-handler.service';
 
