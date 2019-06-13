5d0e72ab60cbe5685834e8d839fa733f016e5228
Evan Trout <evan.trout@sunlife.com>
2019-06-13 15:07:32 -0500

    Expand filterPipe to include all search fields

--- src/main/angular/member-coverage-forms-app/src/app/pipes/filter.pipe.ts
+++ src/main/angular/member-coverage-forms-app/src/app/pipes/filter.pipe.ts
@@ -4,13 +4,25 @@ import { Pipe, PipeTransform } from "@angular/core";
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
-  transform(items: any[], fTypeSearch: string, covTypeSearch: string) {
+  transform(items: any[], fTypeSearch: string, covTypeSearch: string, stateSearch: string[], nameSearch: string, descSearch: string, linkSearch: string) {
     if (items && items.length) {
       return items.filter(item =>{
-        if (fTypeSearch != item.formType){
+        if (fTypeSearch && fTypeSearch != item.formType){
           return false;
         }
-        if (covTypeSearch != item.coverageType){
+        if (covTypeSearch && covTypeSearch != item.coverageType){
+          return false;
+        }
+        if (stateSearch && !stateSearch.includes(item.state)){
+          return false;
+        }
+        if (nameSearch && item.name.toLowerCase().indexOf(nameSearch.toLowerCase()) === -1){
+          return false;
+        }
+        if (descSearch && item.description.toLowerCase().indexOf(descSearch.toLowerCase()) === -1){
+          return false;
+        }
+        if (linkSearch && item.link.toLowerCase().indexOf(linkSearch.toLowerCase()) === -1){
           return false;
         }
         return true;
