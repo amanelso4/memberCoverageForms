26e8aab270e1ead77268549816cfc262104d00b0
KT43 <amanda.x.nelson@sunlife.com>
2019-06-25 14:43:09 -0500

    Merge

---
+++ src/main/java/com/sunlife/groupweb/member/MongoDBLayer/AddSubForm.java
@@ -0,0 +1,14 @@
+package com.sunlife.groupweb.member.MongoDBLayer;
+
+public class AddSubForm {
+
+}
+/*
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public Form addSubForm(@Valid @RequestBody Form form) {
+        // convert Form form into appropriate formatting
+        // add to documents based on state-coverageType-source combo
+    }
+
+ */
+
