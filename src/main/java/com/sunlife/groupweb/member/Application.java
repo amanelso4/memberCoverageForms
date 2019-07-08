229c4a6008519968fa25c8206d39e53d8e0a9825
Evan Trout <evan.trout@sunlife.com>
2019-07-08 13:09:54 -0500

    Set project to generate a WAR file

--- src/main/java/com/sunlife/groupweb/member/Application.java
+++ src/main/java/com/sunlife/groupweb/member/Application.java
@@ -3,17 +3,24 @@ package com.sunlife.groupweb.member;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
+
+/*    @Override
+    public void run(String... args) throws Exception {
+    }*/
 
 }
