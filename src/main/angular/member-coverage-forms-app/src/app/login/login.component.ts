9d9508fb817df17fd848dcc3920aeb2143b25278
KT43 <amanda.x.nelson@sunlife.com>
2019-07-12 12:24:11 -0500

    Initial Login Page complete

--- src/main/angular/member-coverage-forms-app/src/app/login/login.component.ts
+++ src/main/angular/member-coverage-forms-app/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+  get f() { return this.loginForm.controls; }
+
+  username=false;
+  password = false;
+  usernameCheck(usernameCheck) {
+    if(usernameCheck.isEqual('sunlife')) {
+      this.username = true;
+    }
+  }
+ passwordCheck(passwordCheck) {
+    if(passwordCheck.isEqual('sunlife')) {
+      this.password = true;
+    }
+  }
+
+  submissionCheck() {
+    if(this.username === true && this.password == true) {
+      this.submitted = true;
+    }
   }
 
 }
